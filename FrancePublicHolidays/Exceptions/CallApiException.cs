5e6949801651bfb6838d4822e88752855fdd5fb0
BACO Abdelmajid <46558124+AbdelmajidBa@users.noreply.github.com>
2019-11-26 12:21:19 +0100

    Update CallApiException.cs

--- FrancePublicHolidays/Exceptions/CallApiException.cs
+++ FrancePublicHolidays/Exceptions/CallApiException.cs
@@ -2,7 +2,7 @@
 
 namespace FrancePublicHolidays.Exceptions
 {
-    public class CallApiException : Exception
+    internal class CallApiException : Exception
     {
         public CallApiException (string message) : base(message) { }
         public CallApiException(string message, Exception inner) : base(message, inner) { }
