5ad906f5f80e9bc668f518a710c77f693bc5b9c9
BACO Abdelmajid <46558124+AbdelmajidBa@users.noreply.github.com>
2019-11-26 11:58:19 +0100

    Update Holiday.cs

--- FrancePublicHolidays/Model/Holiday.cs
+++ FrancePublicHolidays/Model/Holiday.cs
@@ -1,19 +1,29 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 
 
 namespace FrancePublicHolidays.Model
 {
+     /// <summary>
+    ///  Holi
+    /// </summary>
     public class Holiday 
     {
-
-        
+        /// <summary>
+        /// Public Holiday Nam
+        /// </summary>
         [JsonProperty("nom_jour_ferie")]
         public string Name { get; set; }
-
+        /// <summary>
+        /// Public Holiday Date
+        /// </summary>
         public DateTime Date { get; set; }
 
-
+        /// <summary>
+        /// Determines whether two Holiday instances are equal.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public override bool Equals(object obj)
         {
             if (obj == null) return false;
@@ -24,17 +34,23 @@ namespace FrancePublicHolidays.Model
             return  this.Name == (obj as Holiday).Name &&
                     this.Date.Equals((obj as Holiday).Date);
         }
-
+        /// <summary>
+        /// Default hash function
+        /// </summary>
+        /// <returns></returns>
         public override int GetHashCode()
         {
             return base.GetHashCode();
         }
-
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
-            var h = string.Format("Name= {0}, Date=new DateTime({1}, {2}, {3})}", Name, Date.Year, Date.Month, Date.Day);
+            var holiday = string.Format("Name= \"{0}\", Date=new DateTime({1}, {2}, {3})", Name, Date.Year, Date.Month, Date.Day);
 
-            return "new Holiday {"+ h +"}";
+            return "{" + holiday + "}";
         }
 
     }
